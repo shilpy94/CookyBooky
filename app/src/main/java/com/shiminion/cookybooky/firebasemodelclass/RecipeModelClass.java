b29fb0467dc3bff79dd87ab33b55b23033037f2a
shilpy94 <s.shilpy94@gmail.com>
2018-05-13 00:13:38 +0530

    Firebase Recipe model class

---
+++ app/src/main/java/com/shiminion/cookybooky/firebasemodelclass/RecipeModelClass.java
@@ -0,0 +1,35 @@
+package com.shiminion.cookybooky.firebasemodelclass;
+
+/**
+ * Created by shilpysamaddar on 12/05/18.
+ */
+
+public class RecipeModelClass {
+    private String name;
+    private String url;
+    private String type;
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
