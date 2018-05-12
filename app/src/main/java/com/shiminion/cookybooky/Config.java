f0d83015e5b8d3c812ae8cc8a393e0b40b85d181
shilpy94 <s.shilpy94@gmail.com>
2018-05-13 00:15:21 +0530

    Full App Push

---
+++ app/src/main/java/com/shiminion/cookybooky/Config.java
@@ -0,0 +1,12 @@
+package com.shiminion.cookybooky;
+
+/**
+ * Created by shilpysamaddar on 12/05/18.
+ */
+
+public class Config {
+    public static final String DEVELOPER_KEY = "AIzaSyArLIfFGREtHD3rs-RSkJyArylR_m3uLy4";
+
+    // YouTube video id
+    public static final String YOUTUBE_VIDEO_CODE = "_oEA18Y8gM0";
+}
